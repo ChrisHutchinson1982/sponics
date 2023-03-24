0e0560127d1bc8a43e759fe19c80add3331d9e28
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-24 21:56:53 +0000

    Added CSS for NewButton component

--- components/NewButton/NewButton.js
+++ components/NewButton/NewButton.js
@@ -1,7 +1,9 @@
 export default function NewButton() {
   return (
     <>
-      <div>New Button</div>
+      <button class="px-4 h-12 w-60 py-1 text-2xl text-blue-600 font-bold rounded border border-blue-600 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2">
+        New Sound
+      </button>
     </>
   );
 }
--- components/WordLength/WordLength.js
+++ components/WordLength/WordLength.js
@@ -11,11 +11,11 @@ export default function WordLength() {
       <h2 className="text-1xl p-2 font-bold text-gray-800">Word length?</h2>
       <div className="mb-2 grid grid-cols-4 gap-2">
         {wordLengths.map((length, i) => {
-          const bgColor = length[1] ? "bg-gray-400" : "bg-white";
+          const bgColor = length[1] ? "bg-blue-200" : "bg-white";
 
           return (
             <div
-              className={`flex h-12 w-12 items-center justify-center rounded border border-gray-400 font-bold uppercase text-gray-800 ${bgColor}`}
+              className={`flex h-12 w-12 items-center justify-center rounded border border-blue-600 font-bold uppercase text-gray-800 ${bgColor}`}
             >
               {length[0]}
             </div>
