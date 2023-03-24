b885f63996595f2d9f94779b852abd434884dbac
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-24 21:10:32 +0000

    Added CCS to Sponics header

--- components/WordLength/WordLength.js
+++ components/WordLength/WordLength.js
@@ -1,7 +1,15 @@
 export default function WordLength() {
   return (
-    <>
-      <div>Word Length</div>
-    </>
+    <div className="mb-2 grid grid-cols-4 gap-2">
+      {new Array(4).fill(0).map((_, i) => {
+        return (
+          <div
+            className={
+              "flex h-16 w-16 items-center justify-center border border-gray-400 font-bold uppercase text-white bg-white"
+            }
+          ></div>
+        );
+      })}
+    </div>
   );
 }
