06fdba99be192f08dfa18147820e3917af7ffe4a
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-24 21:19:15 +0000

    Added basic structure and  CCS to WordLength component

--- components/WordLength/WordLength.js
+++ components/WordLength/WordLength.js
@@ -1,15 +1,20 @@
 export default function WordLength() {
   return (
-    <div className="mb-2 grid grid-cols-4 gap-2">
-      {new Array(4).fill(0).map((_, i) => {
-        return (
-          <div
-            className={
-              "flex h-16 w-16 items-center justify-center border border-gray-400 font-bold uppercase text-white bg-white"
-            }
-          ></div>
-        );
-      })}
-    </div>
+    <>
+      <h2 className="text-2xl p-2">Word length?</h2>
+      <div className="mb-2 grid grid-cols-4 gap-2">
+        {new Array(4).fill(0).map((_, i) => {
+          return (
+            <div
+              className={
+                "flex h-12 w-12 items-center justify-center border border-gray-400 font-bold uppercase text-black bg-white"
+              }
+            >
+              {i + 3}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
