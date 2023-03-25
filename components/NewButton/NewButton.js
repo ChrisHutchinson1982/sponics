80cb607c1f414b1ed8e6cd45fb0d29b9c79b64e5
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-25 00:49:37 +0000

    Added bg CSS for Index

--- components/NewButton/NewButton.js
+++ components/NewButton/NewButton.js
@@ -1,8 +1,8 @@
 export default function NewButton() {
   return (
     <>
-      <button class="px-4 h-12 w-60 py-1 text-2xl text-blue-600 font-bold rounded border border-blue-600 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2">
-        New Sound
+      <button class="px-4 h-12 w-24 py-1 mb-6 text-xl text-blue-600 font-bold rounded border border-blue-600 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2">
+        Switch
       </button>
     </>
   );
