cb33c006734d6d6ab251a0e9072454148f2a8e4f
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-26 21:52:49 +0100

    Passed user selects word length feature test to render page with 3 guess letter boxes when word length 3 is clicked

--- cypress/e2e/user_selects_word_length.cy.js
+++ cypress/e2e/user_selects_word_length.cy.js
@@ -1,11 +1,24 @@
 describe("User opens web app to select word length", () => {
-  it("and page shows 4 guess letter boxes as default", () => {
+  it("Page shows 4 guess letter boxes as default", () => {
     cy.visit("http://localhost:3000");
 
     cy.get('[data-cy="guessLetter1"]').should("exist");
     cy.get('[data-cy="guessLetter2"]').should("exist");
     cy.get('[data-cy="guessLetter3"]').should("exist");
     cy.get('[data-cy="guessLetter4"]').should("exist");
+    cy.get('[data-cy="guessLetter5"]').should("not.exist");
+    cy.get('[data-cy="guessLetter6"]').should("not.exist");
+  });
+
+  it("Page shows 3 guess letter boxes when word lenth 3 is selected", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.get('[data-cy="wordLength3"]').click;
+
+    cy.get('[data-cy="guessLetter1"]').should("exist");
+    cy.get('[data-cy="guessLetter2"]').should("exist");
+    cy.get('[data-cy="guessLetter3"]').should("exist");
+    cy.get('[data-cy="guessLetter5"]').should("not.exist");
     cy.get('[data-cy="guessLetter5"]').should("not.exist");
     cy.get('[data-cy="guessLetter6"]').should("not.exist");
   });
