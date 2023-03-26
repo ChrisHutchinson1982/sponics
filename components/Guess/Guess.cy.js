eb543e91e16bfbe42c730ee5f1528068be576d51
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-26 21:34:24 +0100

    Passed Guess unit test to render component with 3 boxes when mounted with 3 wordLength

--- components/Guess/Guess.cy.js
+++ components/Guess/Guess.cy.js
@@ -11,4 +11,15 @@ describe("Guess", () => {
     cy.get('[data-cy="guessLetter5"]').should("not.exist");
     cy.get('[data-cy="guessLetter6"]').should("not.exist");
   });
+
+  it("Renders component with 3 boxes when mounted with 3 wordLength", () => {
+    cy.mount(<Guess wordLength={3} />);
+
+    cy.get('[data-cy="guessLetter1"]').should("exist");
+    cy.get('[data-cy="guessLetter2"]').should("exist");
+    cy.get('[data-cy="guessLetter3"]').should("exist");
+    cy.get('[data-cy="guessLetter4"]').should("not.exist");
+    cy.get('[data-cy="guessLetter5"]').should("not.exist");
+    cy.get('[data-cy="guessLetter6"]').should("not.exist");
+  });
 });
