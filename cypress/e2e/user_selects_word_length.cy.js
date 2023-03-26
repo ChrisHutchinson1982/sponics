d740d056e3e1a8b5ceba75956f6b605d35d047a3
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-26 22:00:19 +0100

    Passed user selects word length feature test to render page with 5 guess letter boxes when word length 5 is clicked

--- cypress/e2e/user_selects_word_length.cy.js
+++ cypress/e2e/user_selects_word_length.cy.js
@@ -13,12 +13,12 @@ describe("User opens web app to select word length", () => {
   it("Page shows 3 guess letter boxes when word length 3 is clicked", () => {
     cy.visit("http://localhost:3000");
 
-    cy.get('[data-cy="wordLength3"]').click;
+    cy.get('[data-cy="wordLength3"]').click();
 
     cy.get('[data-cy="guessLetter1"]').should("exist");
     cy.get('[data-cy="guessLetter2"]').should("exist");
     cy.get('[data-cy="guessLetter3"]').should("exist");
-    cy.get('[data-cy="guessLetter5"]').should("not.exist");
+    cy.get('[data-cy="guessLetter4"]').should("not.exist");
     cy.get('[data-cy="guessLetter5"]').should("not.exist");
     cy.get('[data-cy="guessLetter6"]').should("not.exist");
   });
@@ -26,14 +26,27 @@ describe("User opens web app to select word length", () => {
   it("Page shows 4 guess letter boxes when word length 4 is clicked", () => {
     cy.visit("http://localhost:3000");
 
-    cy.get('[data-cy="wordLength3"]').click;
-    cy.get('[data-cy="wordLength4"]').click;
+    cy.get('[data-cy="wordLength3"]').click();
+    cy.get('[data-cy="wordLength4"]').click();
 
     cy.get('[data-cy="guessLetter1"]').should("exist");
     cy.get('[data-cy="guessLetter2"]').should("exist");
     cy.get('[data-cy="guessLetter3"]').should("exist");
+    cy.get('[data-cy="guessLetter4"]').should("exist");
     cy.get('[data-cy="guessLetter5"]').should("not.exist");
-    cy.get('[data-cy="guessLetter5"]').should("not.exist");
+    cy.get('[data-cy="guessLetter6"]').should("not.exist");
+  });
+
+  it("Page shows 5 guess letter boxes when word length 5 is clicked", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.get('[data-cy="wordLength5"]').click();
+
+    cy.get('[data-cy="guessLetter1"]').should("exist");
+    cy.get('[data-cy="guessLetter2"]').should("exist");
+    cy.get('[data-cy="guessLetter3"]').should("exist");
+    cy.get('[data-cy="guessLetter4"]').should("exist");
+    cy.get('[data-cy="guessLetter5"]').should("exist");
     cy.get('[data-cy="guessLetter6"]').should("not.exist");
   });
 });
