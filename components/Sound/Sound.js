0177af998b45c74895d5055e1aaaf7899175f1f1
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-28 13:58:25 +0100

    Passed sound unit test to Renders component with sound  when mounted

--- components/Sound/Sound.js
+++ components/Sound/Sound.js
@@ -3,6 +3,7 @@ export default function Sound({ sound }) {
     <>
       <div
         className={`flex h-24 w-24 m-4 items-center justify-center rounded border border-blue-600 font-bold text-5xl lowercase text-gray-800 bg-white`}
+        data-cy="sound"
       >
         {sound}
       </div>
