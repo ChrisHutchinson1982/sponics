14069e151a1ebf27c121a01b87ec3624391d7955
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-28 22:24:29 +0100

    Passed WordLength unit test to Renders component with 4 boxes with containing t e s t when mounted with 'test' guess

--- components/Guess/Guess.cy.js
+++ components/Guess/Guess.cy.js
@@ -2,7 +2,7 @@ import Guess from "./Guess";
 
 describe("Guess", () => {
   it("Renders component with 4 boxes when mounted with 4 wordLength", () => {
-    cy.mount(<Guess wordLength={4} />);
+    cy.mount(<Guess wordLength={4} guess={""} />);
 
     cy.get('[data-cy="guessLetter1"]').should("exist");
     cy.get('[data-cy="guessLetter2"]').should("exist");
@@ -13,7 +13,7 @@ describe("Guess", () => {
   });
 
   it("Renders component with 3 boxes when mounted with 3 wordLength", () => {
-    cy.mount(<Guess wordLength={3} />);
+    cy.mount(<Guess wordLength={3} guess={""} />);
 
     cy.get('[data-cy="guessLetter1"]').should("exist");
     cy.get('[data-cy="guessLetter2"]').should("exist");
@@ -24,7 +24,7 @@ describe("Guess", () => {
   });
 
   it("Renders component with 5 boxes when mounted with 5 wordLength", () => {
-    cy.mount(<Guess wordLength={5} />);
+    cy.mount(<Guess wordLength={5} guess={""} />);
 
     cy.get('[data-cy="guessLetter1"]').should("exist");
     cy.get('[data-cy="guessLetter2"]').should("exist");
@@ -35,7 +35,7 @@ describe("Guess", () => {
   });
 
   it("Renders component with 6 boxes when mounted with 6 wordLength", () => {
-    cy.mount(<Guess wordLength={6} />);
+    cy.mount(<Guess wordLength={6} guess={""} />);
 
     cy.get('[data-cy="guessLetter1"]').should("exist");
     cy.get('[data-cy="guessLetter2"]').should("exist");
@@ -43,5 +43,14 @@ describe("Guess", () => {
     cy.get('[data-cy="guessLetter4"]').should("exist");
     cy.get('[data-cy="guessLetter5"]').should("exist");
     cy.get('[data-cy="guessLetter6"]').should("exist");
+  });
+
+  it("Renders component with 4 boxes with containing t e s t when mounted with 'test' guess", () => {
+    cy.mount(<Guess wordLength={4} guess={"test"} />);
+
+    cy.get('[data-cy="guessLetter1"]').should("contain.text", "t");
+    cy.get('[data-cy="guessLetter2"]').should("contain.text", "e");
+    cy.get('[data-cy="guessLetter3"]').should("contain.text", "s");
+    cy.get('[data-cy="guessLetter4"]').should("contain.text", "t");
   });
 });
