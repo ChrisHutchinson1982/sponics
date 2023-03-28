775e0d586e1c128dccd2c40032731869acba6f67
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-28 14:37:43 +0100

    Passed feature test to show random sound as default

---
+++ cypress/e2e/user_gets_random_sound.cy.js
@@ -0,0 +1,13 @@
+import sounds from "../../sounds.json";
+
+describe("User opens web app to select a random sound", () => {
+  it("Page shows a random sound as default", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.get('[data-cy="mainContainer"]').click();
+
+    cy.get('[data-cy="sound"]').each((x) => {
+      expect(x.text()).to.be.oneOf(sounds);
+    });
+  });
+});
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,10 @@ export default observer(function Home() {
   const [wordLength, setWordLength] = useState(4);
   return (
     <>
-      <div className="flex h-screen w-screen flex-col items-center justify-center bg-black">
+      <div
+        className="flex h-screen w-screen flex-col items-center justify-center bg-black"
+        data-cy="mainContainer"
+      >
         <h1 className="bg-gradient-to-br from-blue-600 to-blue-400 bg-clip-text text-8xl font-bold text-transparent p-8">
           Sponics
         </h1>
