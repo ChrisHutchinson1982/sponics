481e33e71c670369e313cf81970daf8b26faa768
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-28 14:12:47 +0100

    Passed sound unit test to Renders component when mounted

--- components/NextButton/NextButton.js
+++ components/NextButton/NextButton.js
@@ -4,8 +4,9 @@ export default observer(function NextButton({ store }) {
   return (
     <>
       <button
-        onClick={store.init}
         className="px-4 h-12 w-24 py-1 mb-6 uppercase text-m text-blue-600 font-bold rounded border border-blue-600 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2"
+        data-cy="nextButton"
+        onClick={store.init}
       >
         Next
       </button>
